118ce9d59761a61bc4ad8013025acfc6bb9d2b85
jvictor-engc <joaovictorbfm.4679@gmail.com>
2025-08-20 11:00:39 -0300

    Update realloc.c
    i improved the code

--- realloc.c
+++ realloc.c
@@ -12,117 +12,103 @@ struct uf {
 
 int main() {
     ///////////////////// para as quantidades
-    FILE *f = fopen("quantidade", "rb+");
-    if (f == NULL) f = fopen("quantidade", "wb+");
-    if (f == NULL) {
+    FILE *fq = fopen("quantidade", "rb+");
+    if (fq == NULL) fq = fopen("quantidade", "wb+");
+    if (fq == NULL) {
         printf("Erro ao abrir arquivo estados\n");
         return -1;
     }
-    /* Função para abrir um arquivo de nome estados, se ele não existir, ele é criado. Caso não seja possível
+    /* Função para abrir um arquivo de nome quantidade, se ele não existir, ele é criado. Caso não seja possível
      * criar, o programa fecha.
      */
 
-    int *p = malloc(sizeof(int));
-    if (p == NULL) {
+    int *q = malloc(sizeof(int));
+    if (q == NULL) {
         printf("Erro ao alocar memoria\n");
-        free(p);// liberação de memória apenas porque a IDE reclama, mas não é necessário já que o programa se fecha ao entrar no if.
+        free(q);// liberação de memória apenas porque a IDE reclama, mas não é necessário já que o programa se fecha ao entrar no if.
         return -1;
     }
 
-    fseek(f, 0, SEEK_SET);
-    if (fread(p, sizeof(int), 1, f) == 0) {
-        *p = 0;
-    }// p guardará quantidade de ufs
+    fseek(fq, 0, SEEK_SET);
+    if (fread(q, sizeof(int), 1, fq) == 0) {
+        *q = 0;
+    }// q aponta para espaço que guarda quantidade de ufs
 
     /////////////////////////////////////////// para as informações das ufs
-    FILE *r = fopen("ufss", "rb+");
-    if (r == NULL) r = fopen("ufss", "wb+");
-    if (r == NULL) {
+    FILE *fu = fopen("ufss", "rb+");
+    if (fu == NULL) fu = fopen("ufss", "wb+");
+    if (fu == NULL) {
         printf("Erro ao abrir arquivo UFs\n");
         return -1;
     }
 
-    struct uf **q;
-    if (*p == 0) q = NULL;
+    struct uf *u;
+    if (*q == 0) u = NULL;
     else {
-        q = malloc(*p * sizeof(struct uf*));
-        if (q == NULL) {
+        u = malloc(*q * sizeof(struct uf));
+        if (u == NULL) {
             printf("Erro ao alocar memoriaaaaaaaaaaaa\n");
+            free(u);
             return -1;
         }
-        for (int i = 0; i < *p; i++) {
-            q[i] = malloc(sizeof(struct uf));
-            if (q[i] == NULL) {
-                printf("Erro ao alocar memoria\n");
-                return -1;
-            }
-        }
     }
 
 
-    fseek(r, 0, SEEK_SET);
-    for (int i = 0; i < *p; i++) {
-        fread(q[i], sizeof(struct uf), 1, r);
-    }
 
-    adicionar( &q, p );
-    printf("%d", *p);
+    fseek(fu, 0, SEEK_SET);
+    fread(u, sizeof(struct uf), *q, fu);
+
+
+    adicionar( &u, q );
     // printf("codigo: %d\n", q);
 
-    mostrar(&q, p);
+    mostrar(u, q);
 
     //////////////////////////////////// fechando e salvando
-    fseek(r, 0, SEEK_SET);
-    for (int i = 0; i < *p; i++) {
-        fwrite(q[i], sizeof(struct uf), 1, r);
+    fseek(fu, 0, SEEK_SET);
+    for (int i = 0; i < *q; i++) {
+        fwrite(u + i, sizeof(struct uf), 1, fu);
     }
-    fseek(f, 0, SEEK_SET);
-    fwrite(p, sizeof(int), 1, f);
+    fseek(fq, 0, SEEK_SET);
+    fwrite(q, sizeof(int), 1, fq);
 
-    fclose(r);
-    fclose(f);
-    free(p);
-    for (int i = 0; i < *p; i++) {
-        free(q[i]);
-    }
+    fclose(fu);
+    fclose(fq);
+    free(u);
     free(q);
+
     return 0;
 
 }
 
-void adicionar( struct uf * **c, int *tam) {// irá mexer no valor real
+void adicionar( struct uf * *c, int *tam) {// irá mexer no valor real. obs: na função eu mexo no endereço de
     (*tam)++;
     printf("%d\n", *tam);
 
-    struct uf **teste = realloc(*c, (*tam) * sizeof(struct uf*));
+    struct uf *teste = realloc(*c, (*tam) * sizeof(struct uf));
     if (teste == NULL) {
         printf("Erro ao realocar memoria\n");
         return;
     }
     *c = teste;
 
-    (*c)[(*tam) - 1] = malloc(sizeof(struct uf));// malloc para a nova uf
-    if ((*c)[(*tam) - 1] == NULL) {
-        printf("Erro ao alocar memória para nova UF\n");
-        return;
-    }
 
 
     printf("Digite o codigo da UF: ");
-    scanf("%d", &((*c)[(*tam)-1]->codigo));
+    scanf("%d", &((*c + (*tam - 1))->codigo));
     /* Aqui deve haver uma análise minuciosa: (*c) == q, (*c)[(*tam) - 1] entra em qual posição de interesse
      * do vetor. Sendo essa última parte um endereço, usamos -> para entrar em código, que é um inteiro, por isso
      * o &*/
 
     printf("Digite a sigla: ");
     fflush(stdin);// Evitar problemas com buffer.
-    fgets((*c)[(*tam)-1]->sigla, 3, stdin);
+    fgets((*c + (*tam - 1))->sigla, 3, stdin);
 
     printf("Pessoa adicionada com sucesso\n");
 }
 
-void mostrar(struct uf * **c, int *tam) {
+void mostrar(struct uf *c, int *tam) {
     for (int i = 0; i < *tam; i++) {
-        printf("UF[%d]: %s\n", (*c)[i]->codigo, (*c)[i]->sigla);
+        printf("UF[%d]: %s\n", c[i].codigo, c[i].sigla);
     }
 }
